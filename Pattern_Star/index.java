4955e13bc07e8a45152810f9875095e1df272ec5
satyamkumar905 <satyambhagat905@gmail.com>
2025-02-01 23:59:00 +0530

    Stars_Print

---
+++ Pattern_Star/index.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class index {
+    public static void main(String[] args) {
+        System.out.println("*****");
+        System.out.println("*****\n*****\n*****\n*****\n*****");
+        System.out.println("*");
+        System.out.println("*****");
+        System.out.println("**");
+    }
+    
+}
