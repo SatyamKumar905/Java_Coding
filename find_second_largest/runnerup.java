62a967d17aff2d1d601e651dbf7ded776fa0b781
satyamkumar905 <satyambhagat905@gmail.com>
2025-02-13 03:55:23 +0530

    Second Largest Number

---
+++ find_second_largest/runnerup.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class runnerup {
+   public static void main(String[] args) {
+       Scanner scn = new Scanner(System.in);
+       
+       System.out.println("Enter the First Number : ");
+       int A = scn.nextInt();
+
+       System.out.println("Enter the second number : ");
+       int B = scn.nextInt();
+
+       System.out.println("Enter the third number : ");
+       int C = scn.nextInt();
+
+       System.out.println("Second largest number IS : ");
+       if(A > B && A > C){
+         if(B > C){
+            System.out.println(B);
+         }else{
+            System.out.println(C);
+         }
+       }else if(B > C){
+         if(A > C){
+            System.out.println(A);
+         }else{
+            System.out.println(C);
+         }
+       }else if(A > B){
+         System.out.println(A);
+       }else{
+         System.out.println(B);
+       }
+    
+   } 
+}
