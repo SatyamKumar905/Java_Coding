e0ec7e2145f14e0ed8ef6e6287646f52c682ec1f
satyamkumar905 <satyambhagat905@gmail.com>
2025-02-13 08:41:33 +0530

    Concatenate_Two_String

---
+++ Concatenate_Two_Strings/index.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class index {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter the first Character of string : ");
+        String first = scn.nextLine();
+
+        System.out.println("Enter the second Character of string : ");
+        String second = scn.nextLine();
+
+        System.out.println(first +" "+ second);
+    }
+}
