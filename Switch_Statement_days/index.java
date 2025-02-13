05bf4895ca7612aa50f66fd4163df0422677bf1a
satyamkumar905 <satyambhagat905@gmail.com>
2025-02-13 06:29:30 +0530

    Days_Print

---
+++ Switch_Statement_days/index.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.io.*;
+
+public class index {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter the Days : ");
+        int day = scn.nextInt();
+
+        switch (day) {
+            case 1:
+            System.out.println("MONDAY");
+            break;
+            case 2:
+            System.out.println("TUESDAY");
+            break;
+            case 3:
+            System.out.println("WEDNESDAY");
+            break;
+            case 4:
+            System.out.println("THURSDAY");
+            break;
+            case 5:
+            System.out.println("FRIDAY");
+            break;
+            case 6:
+            System.out.println("SATURDAY");
+            break;
+            case 7:
+            System.out.println("SUNDAY");
+            break;
+            default:
+            System.out.println("No Match : ");
+            
+        }
+      }
+    
+    }
+
+//   int day = scn.nextInt();
+// switch (day) {
+//   case 1:
+//     System.out.println("Monday");
+//      break;
+//   case 2:
+//     System.out.println("Tuesday");
+//      break;
+//   case 3:
+//     System.out.println("Wednesday");
+//      break;
+//   case 4:
+//     System.out.println("Thursday");
+//        break;
+//   case 5:
+//     System.out.println("Friday");
+//        break;
+//   case 6:
+//     System.out.println("Saturday");
+//      break;
+//   case 7:
+//     System.out.println("Sunday");
+//     break;
+
+//      }
