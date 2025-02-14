3f4c3a9c11588e785fad9867748602dc37a60296
satyamkumar905 <satyambhagat905@gmail.com>
2025-02-14 06:34:59 +0530

    Jumping_Character

---
+++ Jumping_Character/index.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class index {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter the Character number : ");
+        char ch = scn.next().charAt(0);
+        
+        if(ch >= 'a' && ch <= 'w'){
+            System.out.println((char)(ch + 3));
+        }else if(ch >= 'D' && ch <= 'Z'){
+            System.out.println((char)(ch - 3));
+        }else{
+            System.out.println("Can't jump");
+        }
+    }
+}
