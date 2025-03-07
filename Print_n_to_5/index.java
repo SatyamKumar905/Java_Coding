f652b389b3254ec34349d028657d18bbde207378
satyamkumar905 <satyambhagat905@gmail.com>
2025-03-07 15:23:44 +0530

    print of 5 to n

---
+++ Print_n_to_5/index.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class index {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        
+        System.out.println("Enter the number's : N : ");
+        int n = scn.nextInt();
+
+        System.out.println("This answer : ");
+        for(int i = n; i >= 1; i --){
+            System.out.print(i +"\t");
+        }
+    }
+}
