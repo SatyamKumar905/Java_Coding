3c9028e1099f7ddebb7026b10d4b995a7f335b9c
satyamkumar905 <satyambhagat905@gmail.com>
2025-03-12 18:02:45 +0530

    Fibonacci_number_12

---
+++ Fibonacci_number_12/index.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class index {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter the number : ");
+        int n = scn.nextInt();
+
+        int p1 = 0;
+        int p2 = 1;
+        int p3 = 1;
+
+        for(int i = 1; i <= n; i ++){
+            
+            p3 = p1 + p2;
+
+            p2 = p1;
+            p1 = p3;
+
+            System.out.print(p2+ "\t");
+        }
+
+    }
+}
