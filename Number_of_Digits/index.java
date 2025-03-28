729574614a296b237ed5b9358d2fc6a7972e00b7
satyamkumar905 <satyambhagat905@gmail.com>
2025-03-28 06:38:39 +0530

    Number of Digits

---
+++ Number_of_Digits/index.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class index {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the digit : ");
+        int n = scn.nextInt();
+        int digit = 0;
+        while (n > 0){
+            digit ++;
+            n = n / 10;
+        }
+        System.out.println(digit);
+    }
+}
