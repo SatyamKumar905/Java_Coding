56ffaecfef93117769e66a91ecb76a4e4928491f
satyamkumar905 <satyambhagat905@gmail.com>
2025-04-09 13:18:02 +0530

    print change for big Ladder

--- Print_Big_Ladder/index.java
+++ Print_Big_Ladder/index.java
@@ -4,15 +4,17 @@ import java.io.*;
 public class index {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the number of Col : ");
+        int m = scn.nextInt();
         System.out.println("Enter the number of Row : ");
         int n = scn.nextInt();
 
         for(int row = 1; row <= n; row ++){
-            for(int col = 1; col <= n; col ++){
-                if(col == 1|| col == n|| row % 2 == 1){
-                    System.out.print("* ");
+            for(int col = 1; col <= m; col ++){
+                if(col == 1|| col == m|| row % 3 == 1){
+                    System.out.print("*\t");
                 }else{
-                    System.out.print("  ");
+                    System.out.print(" \t");
                 }
             }
             System.out.println();
