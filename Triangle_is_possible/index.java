b3378879e81f6fa9be69c20ed641c1775d0401ea
satyamkumar905 <satyambhagat905@gmail.com>
2025-04-29 12:20:53 +0530

    Triangle_is_possible

--- Triangle_is_possible/index.java
+++ Triangle_is_possible/index.java
@@ -1,20 +1,47 @@
+// import java.util.*;
+// import java.io.*;
+
+// public class index {
+//     public static void main(String[] args) {
+//         Scanner scn = new Scanner(System.in);
+//         System.out.println("Enter the A : ");
+//         int a = scn.nextInt();
+//         System.out.println("Enter the B : ");
+//         int b = scn.nextInt();
+//         System.out.println("Enter the C : ");
+//         int c = scn.nextInt();
+
+//         if(a + b > c){
+//             System.out.println("true");
+//         }else{
+//             System.out.println("false");
+//         }
+//     }
+// }
+
 import java.util.*;
 import java.io.*;
 
 public class index {
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        System.out.println("Enter the A : ");
-        int a = scn.nextInt();
-        System.out.println("Enter the B : ");
-        int b = scn.nextInt();
-        System.out.println("Enter the C : ");
-        int c = scn.nextInt();
-
+    public static boolean sum(int a, int b, int c) {
         if(a + b > c){
-            System.out.println("true");
+            return true;
         }else{
-            System.out.println("false");
+            return false;
         }
     }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        
+        System.out.println("Enter the first Number : ");
+        int a = scn.nextInt();
+        System.out.println("Enter the second number : ");
+        int b = scn.nextInt();
+        System.out.println("Enter the third number : ");
+        int c = scn.nextInt();
+
+        boolean ans = sum(a, b, c);
+        System.out.println(ans);
+    }
+
 }
