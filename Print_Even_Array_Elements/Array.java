ead22bcbcd5a4a0d98f04b00f35f39a728d2938f
satyamkumar905 <satyambhagat905@gmail.com>
2025-06-05 06:08:19 +0530

    Print Even Array Elements inline

---
+++ Print_Even_Array_Elements/Array.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+// package Print_Even_Array_Elements;
+
+public class Array {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter the number of N : ");
+        int n = scn.nextInt();
+        int arr[] = new int[n];
+         for(int i = 0; i < n; i ++){
+            arr[i] = scn.nextInt();
+
+         }
+         for(int i = 0; i < n; i ++){
+            if(arr[i] % 2 == 0){
+                System.out.println(arr[i]);
+            }
+         }
+    }
+}
